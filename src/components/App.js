2564559d14888a9d74ccf25f00558e124d834afc
Swati Suman <104189142+swatisuman27@users.noreply.github.com>
2022-08-30 09:45:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
+import "./styles.css";
+
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
-import ClickButton from './../components/Create_button';
+
 
 function App() {
+  const [state, setState] = useState(false);
+
+  function handleClick(){
+    setState(true)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
-    <ClickButton/>
+        <button id='click' onClick={handleClick} >Click</button>
+      {state ?    <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null }
+
+    
+	   
     </div>
   );
 }
